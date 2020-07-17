5527fada51026e76260364b38221ac81a0dfa69c
Werner Wessely <wessely.werner@gmail.com>
2020-07-17 16:00:17 +0200

    Added Person ctors

--- Person.cpp
+++ Person.cpp
@@ -5,6 +5,10 @@ class Person
 {
     std::string name{};
     std::string addr{};
+
+public:
+    Person() = default;
+    Person(const std::string &name, const std::string addr) : name(name), addr(addr) {}
 };
 
 int main()
